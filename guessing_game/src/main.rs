2bbe9c894fa486c911257b5ea652ce5ac67b3f5e
eayllon1 <eayllon1@gmail.com>
2019-04-04 21:19:45 -0400

    Update main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,10 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
@@ -12,4 +15,6 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    println!("The secret number is: {}", secret_number);
 }
